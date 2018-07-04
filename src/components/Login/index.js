0334229e2f52a207eb352b84995f6f1ba0f16ea7
Vadim <vadimnikee@yandex.ru>
2018-07-04 12:25:39 +0300

    added component

---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class Login extends React.Component{
+  render(){
+    return(
+      <div className="login">
+        Here will be login
+        and more there!!!
+      </div>
+    )
+  }
+}
